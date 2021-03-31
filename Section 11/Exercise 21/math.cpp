1411fc29a17184d2e495190c8819a2ed52fd4a12
Suresh Yhap <sureshyhap@gmail.com>
2021-03-31 10:12:12 -0400

    Finished up to Exercise 23

---
+++ Section 11/Exercise 21/math.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void c_math_functions() {
+    
+    double bill_total {102.78};
+    int number_of_guests {5};
+    
+    //DO NOT MODIFY THE CODE ABOVE THIS LINE----
+    //----WRITE YOUR CODE BELOW THIS LINE----
+    
+    double individual_bill = bill_total / number_of_guests;
+    double individual_bill_1 = floor(individual_bill);
+    double individual_bill_2 = round(individual_bill);
+    double individual_bill_3_temp = individual_bill * 100;
+    individual_bill_3_temp = ceil(individual_bill_3_temp);
+    double individual_bill_3 = individual_bill_3_temp / 100;
+    
+    //----WRITE YOUR CODE ABOVE THIS LINE----
+    //----DO NOT MODIFY THE CODE BELOW THIS LINE----
+    cout << "The individual bill at location 1 will be $" << individual_bill_1 << "\n" << "The individual bill at location 2 will be $" << individual_bill_2 << "\n" << "The individual bill at location 3 will be $" << individual_bill_3;
+}
---
+++ Section 11/Exercise 22/temps.cpp~
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+//----DO NOT MODIFY THE CODE ABOVE THIS LINE----
+//----WRITE YOUR FUNCTION PROTOTYPES BELOW THIS LINE----
+
+
+
+//----WRITE YOUR FUNCTION PROTOTYPES ABOVE THIS LINE----
+//----DO NOT MODIFY THE CODE BELOW THIS LINE----
+
+void temperature_conversion(double fahrenheit_temperature) {
+    
+    //----DO NOT MODIFY THE CODE ABOVE THIS LINE----
+    //----WRITE YOUR FUNCTION CALLS BELOW THIS LINE----
+    
+    
+    
+    //----WRITE YOUR FUNCTION CALLS ABOVE THIS LINE----
+    //----DO NOT MODIFY THE CODE BELOW THIS LINE----
+    
+    cout << "The fahrenheit temperature " << fahrenheit_temperature << " degrees is equivalent to " << celsius_temperature << " degrees celsius and " << kelvin_temperature << " degrees kelvin.";
+}
+
+//----DO NOT MODIFY THE CODE ABOVE THIS LINE----
+//----WRITE YOUR FUNCTION DEFINITIONS BELOW THIS LINE----
+
+
